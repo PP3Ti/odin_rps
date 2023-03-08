76758f15d7d42c80c2e88324dfc75a5bb64f0aa9
PP3Ti <porkolab.peti@gmail.com>
2023-03-08 19:45:36 +0100

    add functionality to play a round

--- rockpaper.js
+++ rockpaper.js
@@ -1,1 +1,45 @@
-console.log("hello");
+function getComputerChoice() {
+
+    let choice = Math.floor(Math.random() * 3); // generate number between 0 and 2
+    if (choice == 0){ 
+        return "rock"
+    } else if (choice == 1) {
+        return "paper"
+    } else {
+        return "scissors"
+    } 
+}
+
+function getPlayerChoice() {
+
+    let selection = prompt("Enter your choice");  // get choice from prompt
+    selection = selection.toLowerCase();    // decapitalize input
+
+    while(1){
+        switch (selection) {                         //force valid input from user
+            case 'rock' : return selection; break;
+            case 'paper': return selection; break;
+            case 'scissors': return selection; break;
+            default : selection = prompt("Please choose a valid tool");
+        }
+    }
+}
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = getPlayerChoice();        // get input from both parties
+    computerSelection = getComputerChoice(); 
+
+    if(playerSelection == computerSelection){       // create logic
+        return "Draw!"
+    } else if ((computerSelection == "rock") && (playerSelection == "scissors")) {
+        return "The computer won!"
+    } else if ((computerSelection == "paper") && (playerSelection == "rock")) {
+        return "The computer won!"
+    } else if ((computerSelection == "scissors") && (playerSelection == "paper")) {
+        return "The computer won!"
+    } else {
+        return "The player won"
+    }
+}
+
+console.log(playRound());
